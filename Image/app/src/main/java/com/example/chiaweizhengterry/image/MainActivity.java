36144b7b3f8911c8ffa25b21597e39907e779471
Terry Chia <chiawz@hotmail.com>
2017-04-11 21:35:56 +0800

    Transition using alpha

--- Image/app/src/main/java/com/example/chiaweizhengterry/image/MainActivity.java
+++ Image/app/src/main/java/com/example/chiaweizhengterry/image/MainActivity.java
@@ -2,8 +2,19 @@ package com.example.chiaweizhengterry.image;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view) {
+
+        ImageView bart = (ImageView) findViewById(R.id.bart);
+        bart.animate().alpha(0).setDuration(2000);
+
+        ImageView homer = (ImageView) findViewById(R.id.homer);
+        homer.animate().alpha(1f).setDuration(2000);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
