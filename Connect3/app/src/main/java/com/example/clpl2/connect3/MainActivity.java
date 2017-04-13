f0c3d0913259b59a807d3e2baa60c8d9a94ee5a0
terry__ <terry_chia@ctl.creative.com>
2017-04-13 10:38:59 +0800

    Connect3 tutorial completed

--- Connect3/app/src/main/java/com/example/clpl2/connect3/MainActivity.java
+++ Connect3/app/src/main/java/com/example/clpl2/connect3/MainActivity.java
@@ -14,17 +14,21 @@ public class MainActivity extends AppCompatActivity {
     //yello = 0, red = 1
     int activePlayer = 0;
 
+    boolean gameIsActive = true;
+
     int[] gameState = {2,2,2,2,2,2,2,2,2};
 
     int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     public void dropIn(View view) {
+
         ImageView counter = (ImageView) view;
-        view.setTranslationY(-1000f);
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tappedCounter] == 2) {
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
+
+            view.setTranslationY(-1000f);
 
             gameState[tappedCounter] = activePlayer;
 
@@ -58,6 +62,28 @@ public class MainActivity extends AppCompatActivity {
 
                     GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
                     gridLayout.setVisibility(View.INVISIBLE);
+
+                    gameIsActive = false;
+
+                } else {
+
+                    boolean gameIsOver = true;
+
+                    for (int counterState : gameState) {
+                        if (counterState == 2) gameIsOver = false;
+                    }
+
+                    if (gameIsOver) {
+                        TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+                        winnerMessage.setText("Game is over!");
+
+                        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                        layout.setVisibility(View.VISIBLE);
+
+                        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+                        gridLayout.setVisibility(View.INVISIBLE);
+                    }
+
                 }
             }
 
@@ -66,6 +92,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void playAgain(View view) {
+
+        gameIsActive = true;
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
         layout.setVisibility(View.INVISIBLE);
@@ -78,8 +106,6 @@ public class MainActivity extends AppCompatActivity {
         for(int i =0 ; i < gameState.length;i++) {
             gameState[i] = 2;
         }
-
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
 
         for (int i = 0 ; i < gridLayout.getChildCount() ; i++) {
 
