9968ec34598c461dfb3fc20622d0bf23a722ae4d
terry__ <terry_chia@ctl.creative.com>
2017-04-20 15:42:55 +0800

    Add new project phrases

---
+++ Phrases/app/src/main/java/com/example/clpl2/phrases/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.clpl2.phrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+        int resourceId = getResources().getIdentifier(ourId,"raw","com.example.clpl2.phrases");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+        Log.i("button Tapped", ourId);
+
+    }
+}
